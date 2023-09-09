a695b91186087de7df222fa8b59fcd1630d5640c
Hiagho <jhiagho@gmail.com>
2023-09-09 15:00:00 -0300

    side_bar_finalizado

--- paginas/main.php
+++ paginas/main.php
@@ -2,30 +2,71 @@
 
 <html>
     <head>
-    <meta charset="utf-8">
+        <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Solve Link</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="http://localhost/ProjetoTCC/css/painel.css">
+        <link rel="stylesheet" href="http://localhost/ProjetoTCC/fontawesome/css/all.min.css">
+        <link rel="stylesheet" href="../">
     </head>
+   
     <body>
+    <article class="menu-principal" >
+                  
+            <aside class="side-bar">
+                <section class="container-image">
+                    <i class="fa-solid fa-user"></i>
+                </section><!--container-image-->
 
-        <aside>
+                <section class="subtitulo-img">
+                    <h1> <?php echo $_SESSION['usuario']; ?></h1>
+                    <h1> <?php echo $_SESSION['permissao']; ?></h1>
+                </section>
 
-        </aside>
+                <ul class="opcoes-menus">
+                    <li>
+                        <img src="./painel/imagens/editar-cadastro.png">
+                        <a href="#">Editar Usuario</a>
+                    </li>
 
-        <?php 
-            if (isset($_GET['loggout'])) {
-                Painel::loggout();
-            }
-            echo $_SESSION['usuario']. '<hr>';
-            echo $_SESSION['permissao']. '<hr>';
-            echo $_SESSION['setor']. '<hr>';
+                    <li>
+                        <img src="./painel/imagens/meu-chamados_2.png">
+                        <a href="#">Meus chamados</a>
+                    </li>
 
-        ?>
-        <h1>Voltar para pagina de login</h1><a href=""></a>
-        <a href="<?php echo INCLUDE_PATH ?>/?loggout"> <i class="fa-solid fa-right-from-bracket"><span>Sair</span></i></a>
+                    <li>
+                        <img src="./painel/imagens/chamados.png">
+                        <a href="#">Chamados</a>
+                    </li>
+                    <li>
+                        <img src="./painel/imagens/base-conhecimento.png">
+                        <a href="#">Base de conhecimento</a>
+                    </li>
+
+                    <li>
+                        <img src="./painel/imagens/Sair.png">
+                        <a href="<?php echo INCLUDE_PATH ?>/?loggout">Sair</a>
+                    </li>
+                </ul>
+                
+            </aside><!--menu-lateral-->
+
+            <section class="conteudo">
+
+                <h2>Testes</h2>
+                <?php 
+                if (isset($_GET['loggout'])) {
+                    Painel::loggout();
+                }
+                echo $_SESSION['usuario']. '<hr>';
+                echo $_SESSION['permissao']. '<hr>';
+                echo $_SESSION['setor']. '<hr>';
+
+                ?>
+            </section>
+        </article>
 
     </body>
 </html>
